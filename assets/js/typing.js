176ce38ce69cf732f29281856394b145170f99e0
Salanzhii Vlad <vladsa1991@gmail.com>
2021-08-30 02:33:44 +0300

    up

--- assets/js/typing.js
+++ assets/js/typing.js
@@ -21,7 +21,7 @@ TxtRotate.prototype.tick = function() {
     this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
 
     var that = this;
-    var delta = 300 - Math.random() * 100;
+    var delta = 100 - Math.random() * 100;
 
     if (this.isDeleting) { delta /= 2; }
 
@@ -51,6 +51,5 @@ window.onload = function() {
     // INJECT CSS
     var css = document.createElement("style");
     css.type = "text/css";
-    css.innerHTML = ".txt-rotate > .wrap { border-right: 0.08em solid #666 }";
     document.body.appendChild(css);
 };
